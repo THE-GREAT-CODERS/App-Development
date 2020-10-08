1ec57200c4fbbeca6a95cefa4976e343fa65dc75
Shaman07 <71549756+Shaman07@users.noreply.github.com>
2020-10-08 15:35:03 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,29 @@
+var player, playerI;
+var obstacles,obstaclesI,obstaclesII,obstaclesIII,obstaclesIV;
 
+
+function preload() {
+  playerI=loadImage();
+   obstaclesI=loadImage();
+   obstaclesII=loadImage();
+  obstaclesIII=loadImage();
+  obstaclesIV=loadImage();
+  }
+function setup() {
+ player=createSprite();
+ player.addImage();
+ //controls
+if(keyDown(UP_ARROW)){
+player.velocityY=-3;
+}  
+if(keyDown(LEFT_ARROW)){
+player.velocityX=-3;
+}  
+  if(keyDown(UP_ARROW)){
+player.velocityY=10;
+}  
+  if(keyDown(UP_ARROW)){
+player.velocityY=-10;
+}  
+  
+}
